2c0bddd6a2e5a445e9dabfff994bb3e0fe7f3917
Matthew P. Chicano <matthew.poole485@gmail.com>
2025-10-06 10:43:14 -0400

    Updated rclone.sh

--- rclone.sh
+++ rclone.sh
@@ -1,41 +1,18 @@
 #!/bin/bash
 
-# Defining variables
-STORAGE="/run/media/mattastic/7d14135c-9d09-4373-bc72-613c6dd992de"
-ONEDRIVE="$STORAGE/OneDrive"
-GOOGLEDRIVE="$STORAGE/GoogleDrive"
-TRIES=5
-DELAY=10
+# Defining variable
+ONEDRIVE="~/OneDrive"
 
-# Checking if Storage Drive is mounted
-while ! findmnt -M "$STORAGE" > /dev/null; do
-	sleep $DELAY
-	TRIES=$((TRIES -1))
-
-	if [ $TRIES -le 0 ]; then
-		xcowsay --time=0 --release "I am Error: Stooooorage Drive is not mounted!"&
-		exit
-	fi
-done
-
-# Creating cloud directories if they doesn't exist
+# Ensuring directory exists
 mkdir -p "$ONEDRIVE"
-mkdir -p "$GOOGLEDRIVE"
 
 # Mounting cloud drives
 rclone --vfs-cache-mode full mount OneDrive: "$ONEDRIVE"&
-rclone --vfs-cache-mode full mount GoogleDrive: "$GOOGLEDRIVE"&
-
 sleep 60s
 
-# Checking if cloud drives properly mounted
+# Checking if drive properly mounted
 if ! findmnt -M "$ONEDRIVE" > /dev/null || ! findmnt -M "$GOOGLEDRIVE" > /dev/null; then
-	if ! findmnt -M "$ONEDRIVE" > /dev/null; then
-		xcowsay --time=0 --release "I am Error: OneDrive failed to connect!"&
-	fi
-	if ! findmnt -M "$GOOGLEDRIVE" > /dev/null; then
-		xcowsay --time=0 --release "I am Error: GoogleDrive failed to connect"&
-	fi
+	xcowsay --time=0 --release "I am Error: OneDrive failed to connect!"&
 else
-	xcowsay "Mooooooo!: OneDrive and GooooooogleDrive successfully connected!"&
+	xcowsay "Mooooooo!: OneDrive successfully mounted!"&
 fi
